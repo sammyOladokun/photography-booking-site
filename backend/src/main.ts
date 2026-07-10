70404f9230b38dc9e551e9ed0242b8122e7f3c33
SammyOladokun <samueltemitopeoladokun@gmail.com>
2026-07-10 10:31:22 +0100

    update

--- backend/src/main.ts
+++ backend/src/main.ts
@@ -3,9 +3,14 @@ import { AppModule } from './app.module';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
+
   app.enableCors();
-  await app.listen(4000);
-  console.log('Backend listening on http://localhost:4000');
+
+  const port = process.env.PORT || 4000;
+
+  await app.listen(port);
+
+  console.log(`Backend listening on port ${port}`);
 }
 
 bootstrap();
